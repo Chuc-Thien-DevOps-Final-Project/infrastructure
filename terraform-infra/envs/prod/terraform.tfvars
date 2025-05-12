c7f9411dd333d4678b96a255fdbc3d7a74dfb1e1
tranchucthien <chucthien2@gmail.com>
2025-05-12 18:51:23 +0700

    add eks

---
+++ terraform-infra/envs/prod/terraform.tfvars
@@ -0,0 +1,26 @@
+# Terraform variables for EKS cluster
+
+
+
+vpc_id       = "vpc-0dd1e20336beb75e5"
+subnet_ids   = [
+  "subnet-0ad1f45dee4200759",
+  "subnet-062c801042588df9a",
+  "subnet-0006f815c074a166b",
+]
+
+cluster_version = "1.32"
+node_groups = {
+  eks_nodes = {
+    instance_types = ["t3.medium"]
+    capacity_type  = "ON_DEMAND"
+    scaling_config = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+    }
+  }
+}
+
+cluster_role_arn = "arn:aws:iam::492804330065:role/thuan_devops_role"
+node_role_arn = "arn:aws:iam::492804330065:role/thuan_devops_role"
---
+++ terraform-infra/envs/prod/variables.tf
@@ -0,0 +1,54 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default = "tct-eks-cluster"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default = "tct-final-project"
+  
+}
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default = "1.32"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs"
+  type        = list(string)
+}
+
+variable "node_groups" {
+  description = "EKS node group configuration"
+  type = map(object({
+    instance_types = list(string)
+    capacity_type  = string
+    scaling_config = object({
+      desired_size = number
+      max_size     = number
+      min_size     = number
+    })
+  }))
+}
+
+
+variable "cluster_role_arn" {
+  description = "IAM role ARN for the EKS cluster"
+  type        = string
+  default =  null
+}
+
+variable "node_role_arn" {
+  description = "IAM role ARN for the EKS node group"
+  type        = string
+    default =  null
+  
+}
